58d2274e1d67ee62e0ab284e1e0203a7956e7efc
stevenyslee <stevenyslee@gmail.com>
2018-06-19 10:15:10 -0700

    Add class groovy dancer

--- src/groovyDancer.js
+++ src/groovyDancer.js
@@ -1,5 +1,6 @@
 var makeGroovyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
+this.$node.addClass('groovyDancer');
 };
 
 makeGroovyDancer.prototype = Object.create(makeDancer.prototype);
@@ -7,5 +8,5 @@ makeGroovyDancer.prototype.constructor = makeGroovyDancer;
 
 makeGroovyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  this.$node.slideToggle();
+  // this.$node.slideToggle();
 };
